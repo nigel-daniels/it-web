24b1d57a44a992f12ee4289468c52e80634daa50
Nigel Daniels <nigel.daniels@initiatethinking.com>
2017-05-26 16:12:22 -0700

    Copyright update

--- config/passport.js
+++ config/passport.js
@@ -1,11 +1,11 @@
 /**
- * Copyright 2015 Axiomode Ltd.
- * Author: Nigel
+ * Copyright 2017 Initiate Thinking
+ * Author: Nigel Daniels
  */
 module.exports = function(passport, passportLocal, models) {
 
 	/* ***************************************
-	 *  Passport login sessions  
+	 *  Passport login sessions
 	 *************************************** */
 
 	// used to serialize the user for the session
@@ -22,9 +22,9 @@ module.exports = function(passport, passportLocal, models) {
 			});
 		});
 
-	
+
 	/* ***************************************
-	 *  LOCAL SIGNUP Strategy  
+	 *  LOCAL SIGNUP Strategy
 	 *************************************** */
 	passport.use('localSignup', new passportLocal(
 		{
@@ -33,24 +33,24 @@ module.exports = function(passport, passportLocal, models) {
 		passwordField : 'password',
 		passReqToCallback:	true		// We need the extra user data for the org and user.
 		},
- 
+
 		function(req, email, password, done) {
 			log.debug('passport.signup, called.');
 			log.debug('passport.signup, req: ' + req + ', email: ' + email + ', done: ' + JSON.stringify(done));
-			
+
 			// check to see if an organisation is specified
 			if (typeof req.body.organisation !== 'undefined')
 				{
 				log.info('passport.signup, organisation provided: ' + req.body.organisation);
 				// First Check the organisation does not already exist
 				models.Organisation.findByName(req.body.organisation, function(err, org) {
-					
+
 					// If there is an error return it
 					if (err) {
 						log.error('passport.signup, error finding org, err: ' + JSON.stringify(err));
 						return done(err);
 						}
-					
+
 					// We got an org so return an error
 					if (org) {
 						log.info('passport.signup, fail organisation exists: ' + org.name);
@@ -58,24 +58,24 @@ module.exports = function(passport, passportLocal, models) {
 					} else {
 						// Create the organisation
 						new models.Organisation.create(req.body.organisation, function(err, newOrg) {
-							
+
 							// If there is an error return it
 							if (err) {
 								log.error('passport.signup, error creating org, err: ' + JSON.stringify(err));
 								return done(err);
 								}
-							
+
 							log.info('passport.signup, org created id: ' + newOrg.id);
-							
+
 							// Check user does not exist
 							models.User.findByEmail(email, function(err, user) {
-								
+
 								// If there is an error return it
 								if (err) {
 									log.error('passport.signup, error finding user, err: ' + JSON.stringify(err));
 									return done(err);
 									}
-									
+
 								// If we got a user return an err
 								if (user) {
 									log.info('passport.signup, fail user exists: ' + user._id);
@@ -102,13 +102,13 @@ module.exports = function(passport, passportLocal, models) {
 
 				// Check user does not exist
 				models.User.findByEmail(email, function(err, user) {
-					
+
 					// If there is an error return it
 					if (err) {
 						log.error('passport.signup, error finding user, err: ' + JSON.stringify(err));
 						return done(err);
 						}
-						
+
 					// If we got a user return an err
 					if (user) {
 						log.info('passport.signup, fail user exists: ' + user._id);
@@ -131,9 +131,9 @@ module.exports = function(passport, passportLocal, models) {
 			})
 		);
 
-	
+
 	/* ***************************************
-	 *  LOCAL LOGIN Strategy  
+	 *  LOCAL LOGIN Strategy
 	 *************************************** */
     passport.use('localLogin', new passportLocal(
     	{
@@ -141,7 +141,7 @@ module.exports = function(passport, passportLocal, models) {
         usernameField : 'email',
         passwordField : 'password'
     	},
-    
+
     	function(email, password, done) { // callback with email and password from our form
     		log.debug('passport.login, called.');
     		// Check the user exists!
@@ -156,8 +156,8 @@ module.exports = function(passport, passportLocal, models) {
 	            if (!user) {
 	            	log.info('passport.login, user not found');
 	            	return done(null, false, {message: 'Email could not be found or the password incorrect.'});
-	            	} 
-	
+	            	}
+
 	            // Check the user is active and the password is ok
 	            if (user.active) {
 		            if (!user.validPassword(password)) {
@@ -168,7 +168,7 @@ module.exports = function(passport, passportLocal, models) {
 	            	return done(null, false, {message: 'This account has been deactivated.'});
 	            	}
 	            // The last two return the same message as we don't want help trangressors
-	            
+
 	            // all is well, return successful user
 	            log.info('passport.login, success');
 	            return done(null, user);
